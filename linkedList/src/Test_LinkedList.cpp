4d3b961747fc0e5ef202b462fc373618ea294d23
pinfante <pasquale.infante@gmail.com>
2018-10-15 12:27:00 -0400

    adding linked list problems

--- linkedList/src/Test_LinkedList.cpp
+++ linkedList/src/Test_LinkedList.cpp
@@ -10,6 +10,8 @@ class TestLinkedList : public Test
 
 	pair<LinkedListNode*, LinkedListNode*> populuate();
 
+	LinkedListNode* createList(const vector<int>& values);
+
 };
 
 pair<LinkedListNode*, LinkedListNode*> TestLinkedList::populuate()
@@ -26,6 +28,16 @@ pair<LinkedListNode*, LinkedListNode*> TestLinkedList::populuate()
 	tail1 = _insert_node_into_singlylinkedlist(head1, tail1, 5);
 	tail2 = _insert_node_into_singlylinkedlist(head2, tail2, 8);
 	return make_pair(head1, head2);
+}
+
+LinkedListNode* TestLinkedList::createList(const vector<int>& values)
+{
+	LinkedListNode *head{nullptr}, *tail{nullptr};
+	head = tail = _insert_node_into_singlylinkedlist(head, tail, values[0]);
+	for(auto it = values.begin()+1; it != values.end(); ++it)
+		tail = _insert_node_into_singlylinkedlist(head, tail, *it);
+
+	return head;
 }
 
 TEST_F(TestLinkedList, verifyValues)
@@ -327,5 +339,115 @@ TEST_F(TestLinkedList, find_repeating1)
 	tail1->next = head1;
 
 	EXPECT_THAT(find_median(start), Eq(6)); 
+}
+
+TEST_F(TestLinkedList, find_middle_node)
+{
+	LinkedListNode *head{nullptr}, *tail{nullptr};
+
+	head = tail = _insert_node_into_singlylinkedlist(head, tail, 1);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 2);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 3);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 4);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 5);
+
+	auto mid = find_middle_node(head);
+	EXPECT_THAT(mid->val, Eq(3));
+
+	tail = _insert_node_into_singlylinkedlist(head, tail, 6);
+	mid = find_middle_node(head);
+	EXPECT_THAT(mid->val, Eq(4));
+}
+
+TEST_F(TestLinkedList, swap_nodes)
+{
+	LinkedListNode *head{nullptr}, *tail{nullptr};
+
+	head = tail = _insert_node_into_singlylinkedlist(head, tail, 1);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 2);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 3);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 4);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 7);
+	tail = _insert_node_into_singlylinkedlist(head, tail, 0);
+
+	auto n{swap_nodes(head, 2)};
+	EXPECT_THAT(n->next->val, Eq(7));
+
+	n = swap_nodes(head, 1);
+	EXPECT_THAT(n->val, Eq(0));
+}
+
+TEST_F(TestLinkedList, reverse_linked_list_in_groups_of_k)
+{
+	auto head = createList(vector<int>{1,2,3,4,5,6});
+	head = reverse_linked_list_in_groups_of_k(head, 3);
+	EXPECT_THAT(head->val, Eq(3));
+	EXPECT_THAT(head->next->val, Eq(2));
+	EXPECT_THAT(head->next->next->val, Eq(1));
+	EXPECT_THAT(head->next->next->next->val, Eq(6));
+	EXPECT_THAT(head->next->next->next->next->val, Eq(5));
+	EXPECT_THAT(head->next->next->next->next->next->val, Eq(4));
+
+	auto h2 = createList(vector<int>{1,2,3,4,5,6,7,8});
+	h2 = reverse_linked_list_in_groups_of_k(h2, 3);
+	EXPECT_THAT(h2->val, Eq(3));
+	EXPECT_THAT(h2->next->val, Eq(2));
+	EXPECT_THAT(h2->next->next->val, Eq(1));
+	EXPECT_THAT(h2->next->next->next->val, Eq(6));
+	EXPECT_THAT(h2->next->next->next->next->val, Eq(5));
+	EXPECT_THAT(h2->next->next->next->next->next->val, Eq(4));
+	EXPECT_THAT(h2->next->next->next->next->next->next->val, Eq(8));
+	EXPECT_THAT(h2->next->next->next->next->next->next->next->val, Eq(7));
+
+	auto h3 = createList(vector<int>{-50,0,50});
+	h3 = reverse_linked_list_in_groups_of_k(h3, 2);
+	EXPECT_THAT(h3->val, Eq(0));
+	EXPECT_THAT(h3->next->val, Eq(-50));
+	EXPECT_THAT(h3->next->next->val, Eq(50));
+
+
+	auto h4 = createList(vector<int>{-20,10,20,-10});
+	h4 = reverse_linked_list_in_groups_of_k(h4, 4);
+	EXPECT_THAT(h4->val, Eq(-10));
+	EXPECT_THAT(h4->next->val, Eq(20));
+	EXPECT_THAT(h4->next->next->val, Eq(10));
+	EXPECT_THAT(h4->next->next->next->val, Eq(-20));
+
+	auto h5 = createList(vector<int>{-10, -20});
+	h5 = reverse_linked_list_in_groups_of_k(h5, 2);
+	EXPECT_THAT(h5->val, Eq(-20));
+	EXPECT_THAT(h5->next->val, Eq(-10));
+
+}
+
+TEST_F(TestLinkedList, alternateSplit)
+{
+	auto [l1, l2] = alternateSplit(createList(vector<int>{1,2,3,4,5,6,7,8}));
+	EXPECT_THAT(l1->val, Eq(1));
+	EXPECT_THAT(l1->next->val, Eq(3));
+	EXPECT_THAT(l1->next->next->val, Eq(5));
+	EXPECT_THAT(l1->next->next->next->val, Eq(7));
+	EXPECT_THAT(l2->val, Eq(2));
+	EXPECT_THAT(l2->next->val, Eq(4));
+	EXPECT_THAT(l2->next->next->val, Eq(6));
+	EXPECT_THAT(l2->next->next->next->val, Eq(8));
+
+	auto [list1, list2] = alternateSplit(createList(vector<int>{1}));
+	EXPECT_THAT(list1->val, Eq(1));
+	EXPECT_THAT(list2, Eq(nullptr));
+}
+
+
+TEST_F(TestLinkedList, mergeSortList)
+{
+	auto head = mergeSortList(createList(vector<int>{100,20,-35,14,51,16,-72,85}));
+	EXPECT_THAT(head->val, Eq(-72));
+	EXPECT_THAT(head->next->val, Eq(-35));
+	EXPECT_THAT(head->next->next->val, Eq(14));
+	EXPECT_THAT(head->next->next->next->val, Eq(16));
+	EXPECT_THAT(head->next->next->next->next->val, Eq(20));
+	EXPECT_THAT(head->next->next->next->next->next->val, Eq(51));
+	EXPECT_THAT(head->next->next->next->next->next->next->val, Eq(85));
+	EXPECT_THAT(head->next->next->next->next->next->next->next->val, Eq(100));
 }
 
